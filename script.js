403746096e06678e9bea43d9219a442492555959
eli <elisa.vassanelli@dci-student.org>
2024-08-30 15:48:51 +0200

    finished profile

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("My first js file!");
+// print to console
+// window.alert("My first js file!");
+// print to browser console
+
+// let variable_name = a value;
+//let my_name = "Eli"; 
+//console.log("Hi hi Eli");
+//console.log("Eli is at home");
+//console.log(my_name + " is learning JavaScript");
+//console.log( my_name + " is having fun");
+
+// let chupa = document.getElementById("red");
+// it means: go to html file and find me the id red
+//console.log("found this element by id");
+//console.log(red);
+
+let allParagraphs = document.getElementsByTagName("p");
+console.log("all paragraph elements");
+console.log(allParagraphs);
+
+let allbluegroup = document.getElementsByClassName("bluegroup");
+console.log("all bluegroup elements");
+console.log(allbluegroup);
